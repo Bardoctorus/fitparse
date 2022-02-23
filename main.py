ca6ca0616a2779a1360a1982836f9c02b171d3ca
Bardoctorus <ian.m.buckley@gmail.com>
2022-02-23 16:37:16 +0100

    fiddle with data, output to file and ignore non number things. Next, set up stepping by timestamp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fitparse import FitFile
 
 
+outputFile = open("out.txt", "a")
+
 fitfile = FitFile('441657585374887937.fit')
 counter =0
 # Get all data messages that are of type record
@@ -8,15 +10,28 @@ for record in fitfile.get_messages('record'):
 
     # Go through all the data entries in this record
     for record_data in record:
-        counter+=1
+        if record_data.name == "activity_type":
+            continue
+
+
+
+
+
         # Print the records name and value (and units if it has any)
         if record_data.units:
-            print(" * %s: %s %s" % (
+#            print(" * %s: %s %s" % (record_data.name, record_data.value, record_data.units,))
+            outputFile.write(" * %s: %s %s\n" % (
                 record_data.name, record_data.value, record_data.units,
             ))
         else:
-            print(" * %s: %s" % (record_data.name, record_data.value))
-    print()
+ #           print(" * %s: %s" % (record_data.name, record_data.value)) 
+            outputFile.write(" * %s: %s\n" % (record_data.name, record_data.value))
+    #print()
 
-
+    outputFile.write("\n")
+    counter+=1
 print(counter)
+
+
+outputFile.close()
+
